a30df908faca41981b85688f74af513e9bd095d8
Sergey Biryukov <2700036@mail.ru>
2020-08-12 23:01:30 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+import {createStore} from 'redux';
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
-      return ++state;
-      
+      return state + 1;
+
+    case 'DEC':
+      return state - 1;
+         
     default:
       return state;
   }
 }
 
-let state = reducer(undefined, {});
-console.log(state);
-state = reducer(state, { type: 'INC' });
-console.log(state);
-state = reducer(state, { type: 'INC' });
-console.log(state);
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'})
+})
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'})
+})
+
+const update = () => {
+  document.getElementById('counter').innerHTML = store.getState();
+}
+
+store.subscribe(update)
+
